d91d339c43c4cd4de534c3a8f7c1100c04c7d7d5
Adam Solove <asolove@gmail.com>
2011-06-28 11:15:55 -0400

    Tests pass jslint.

--- test/tests.js
+++ test/tests.js
@@ -27,21 +27,22 @@ module("Multiple uploads", {
 		data.uploader = $("input#file").data("uploader");
 		data.label = $("." + data.uploader.options.html.addLabelClass);
 	}
-})
+});
 
 test("Below the limit", function(){
 	ok(data.uploader.canAddFile(), "Can add another file below the limit");
 	ok(data.label.is(":visible"), "Add label should be present");
-})
+});
 
 test("When at the limit", function(){
 	data.uploader.addFile();
 	ok(!data.uploader.canAddFile(), "Can't add another file past the limit");
 	ok(!data.label.is(":visible"), "Add label should not be present");
-})
+});
 
 test("UI click event adds another input", function(){
 	var inputs = $("input").length;
 	data.label.click();
 	equals($("input").length, inputs+1);
-})
+});
+
